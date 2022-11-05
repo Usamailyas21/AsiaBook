008ca43b84bb66a6a5a2439ce45c0938389df0fc
Usamailyas21 <103099663+Usamailyas21@users.noreply.github.com>
2022-11-05 15:37:31 +0500

    Admin Change

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -16,8 +16,9 @@ const HomeContainer = () => {
           />
         </div>
       </div>
-        <p className='text-[2.5rem] lg:text-[4rem] font-bold tracking-wide text-headingColor'>
-        السلام عليكم Welcome To <span className='text-orange-600 text-[3rem] lg:text-[5rem]'>Asia Book Depot Kamoke</span></p>
+        <p className='text-[2.5rem] lg:text-[3.5rem] font-bold tracking-wide text-headingColor'>
+        السلام عليكم Welcome To
+        <span className='text-orange-600 text-[3rem] lg:text-[3.5rem]'> Asia Book Depot Kamoke</span></p>
 
         <p className='text-base text-textColor text-center md:text-left md:w-[80%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos reprehenderit veritatis dolore assumenda quaerat cupiditate ut possimus necessitatibus ipsam! Blanditiis, delectus! Animi sunt dolorem rerum quasi ex laborum, sequi quod.</p>
         <button type='button' 
@@ -31,7 +32,7 @@ const HomeContainer = () => {
             {heroData&& heroData.map(n =>(
                 <div key={n.id} className='w-190 min-w-[190px] p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
                     <img src={n.imgSrc} className='w-40 =mt-20' alt="alb" />
-                        <p className='text-xl text-base font-semibold text-textColor mt-4'>{n.name}</p>
+                        <p className='text-xl font-semibold text-textColor mt-4'>{n.name}</p>
                         <p className='text-sm text-gray-500 font-semibold my-3'>{n.decp}</p>
                 </div>
 
